41af0e398a2188deb04d491855c58fa903618766
shiba-x86ken <38150179+shiba-x86ken@users.noreply.github.com>
2018-04-08 16:46:45 +0900

    Add files via upload

---
+++ strings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int num = 0;
+
+    if (num == 0) {
+        printf("Retry...");
+    } else {
+        printf("flag is {You_understand_how_to_use_strings_command}");
+    }
+
+    return 0;
+}
